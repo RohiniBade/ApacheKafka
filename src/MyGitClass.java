12230dfe5bcef69913e9bbc2bd2ed448e400e978
RohiniBade <84895385+RohiniBade@users.noreply.github.com>
2021-05-31 10:53:32 +0530

    Update MyGitClass.java

--- src/MyGitClass.java
+++ src/MyGitClass.java
@@ -4,6 +4,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("MyFirst Git Class");
+		System.out.println("My second Git Statement");
 	}
 
 }
